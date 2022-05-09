a400fc82ee32350aa152dd2ec25ade68d581adb4
Kshitiz Kamal <skilite007@gmail.com>
2022-05-09 10:37:45 +0530

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_art/core/app_widget.dart';
+import 'package:flutter_art/firebase_options.dart';
+import 'package:url_strategy/url_strategy.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  setPathUrlStrategy();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(AppWidget());
 }
